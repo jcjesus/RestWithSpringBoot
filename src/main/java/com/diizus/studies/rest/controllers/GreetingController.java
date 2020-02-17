637ac97e0ecc480982c4c990d8d5834bdaff0571
Jesus <julio.jesus@ilog.com.br>
2020-02-17 14:17:30 -0300

    Greeting first commit.

---
+++ src/main/java/com/diizus/studies/rest/controllers/GreetingController.java
@@ -0,0 +1,21 @@
+package com.diizus.studies.rest.controllers;
+
+import com.diizus.studies.rest.model.Greeting;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+@RestController
+public class GreetingController {
+
+    private static final String template = "hello, %s!";
+
+    private final AtomicLong counter = new AtomicLong();
+
+    @RequestMapping("/greeting")
+    public Greeting greeting(@RequestParam(value="name", defaultValue = "world") String name) {
+        return new Greeting(counter.incrementAndGet(), String.format(template, name));
+    }
+}
